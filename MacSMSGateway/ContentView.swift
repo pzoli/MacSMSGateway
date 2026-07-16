766e93a364a1c2f0196c7810e667590ae641b90b
Papp Zoltan <papp.zoltan@infokristaly.hu>
2026-07-16 19:24:18 +0200

    Initial Commit

---
+++ MacSMSGateway/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacSMSGateway
+//
+//  Created by Papp Zoltán on 2026. 07. 16..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacSMSGateway/MacSMSGatewayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSMSGatewayApp.swift
+//  MacSMSGateway
+//
+//  Created by Papp Zoltán on 2026. 07. 16..
+//
+
+import SwiftUI
+
+@main
+struct MacSMSGatewayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
